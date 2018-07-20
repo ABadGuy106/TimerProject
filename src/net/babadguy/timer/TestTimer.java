a6bf797dcde8b03fab96e667a1f2cff56ccaebe4
liujiwei <liujiwei0529@163.com>
2018-07-20 15:22:51 +0800

    schedule(task,time)函数使用

--- src/net/babadguy/timer/TestTimer.java
+++ src/net/babadguy/timer/TestTimer.java
@@ -1,5 +1,7 @@
 package net.babadguy.timer;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Timer;
 
 public class TestTimer {
@@ -10,6 +12,25 @@ public class TestTimer {
         TestTimerTask testTimerTask=new TestTimerTask("No.1");
         //通过timer定时定评论调用testTimerTask的业务逻辑
         //即第一次执行是再当前时间的两秒之后，只会每隔一秒执行一次
-        timer.schedule(testTimerTask,2000,1000);
+        //timer.schedule(testTimerTask,2000,1000);
+
+        /**
+         * 获取当前时间，并设置成距离当前时间前三秒之后的时间
+         * 如果当前时间是2018-11-10 23：59：57
+         * 则设置后的时间则为2018-11-11 00：00：00
+         */
+        Calendar calendar=Calendar.getInstance();
+        SimpleDateFormat sf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(sf.format(calendar.getTime()));
+        calendar.add(Calendar.SECOND,3);
+        /**
+         * 在时间等于或超过time的时候执行且仅执行一次task
+         */
+        testTimerTask.setName("schedule1");
+        timer.schedule(testTimerTask,calendar.getTime());
+
+
+
+
     }
 }
--- src/net/babadguy/timer/TestTimerTask.java
+++ src/net/babadguy/timer/TestTimerTask.java
@@ -1,5 +1,7 @@
 package net.babadguy.timer;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.TimerTask;
 
 public class TestTimerTask extends TimerTask {
@@ -20,6 +22,10 @@ public class TestTimerTask extends TimerTask {
 
     @Override
     public void run() {
+        //以yyyy-MM-dd HH:mm:ss的格式打印当前执行时间
+        Calendar calendar=Calendar.getInstance();
+        SimpleDateFormat sf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.print("Current exec time is:"+sf.format(calendar.getTime()));
         //打印当前name的内容
         System.out.println("Current exec name is:"+name);
     }
