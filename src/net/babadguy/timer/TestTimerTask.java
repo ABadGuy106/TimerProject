54d989b5cec1d06fcef13bdb7b82421913cf9d9e
liujiwei <liujiwei0529@163.com>
2018-07-20 16:32:35 +0800

    cancle()函数的使用

--- src/net/babadguy/timer/TestTimerTask.java
+++ src/net/babadguy/timer/TestTimerTask.java
@@ -8,6 +8,8 @@ public class TestTimerTask extends TimerTask {
 
     private String name;
 
+    private Integer count=0;
+
     public String getName() {
         return name;
     }
@@ -22,11 +24,18 @@ public class TestTimerTask extends TimerTask {
 
     @Override
     public void run() {
-        //以yyyy-MM-dd HH:mm:ss的格式打印当前执行时间
-        Calendar calendar=Calendar.getInstance();
-        SimpleDateFormat sf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        System.out.print("Current exec time is:"+sf.format(calendar.getTime()));
-        //打印当前name的内容
-        System.out.println("Current exec name is:"+name);
+        if(count<3){
+            //以yyyy-MM-dd HH:mm:ss的格式打印当前执行时间
+            Calendar calendar=Calendar.getInstance();
+            SimpleDateFormat sf=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            System.out.print("Current exec time is:"+sf.format(calendar.getTime()));
+            //打印当前name的内容
+            System.out.println("Current exec name is:"+name);
+            count++;
+        }else {
+            cancel();
+            System.out.println("Task cancel!");
+        }
+
     }
 }
